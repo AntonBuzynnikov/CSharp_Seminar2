ac887758205f1a02ab7a2a9e79bb713ebf116f1e
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-31 17:51:07 +0700

    Complete Example023

---
+++ Example023/Program.cs
@@ -0,0 +1,7 @@
+﻿//                                         r1        r2
+//23. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y 
+//!(X || Y)==!X && !Y
+bool x = false;
+bool y = false;
+System.Console.Write("Выражение ¬(X ⋁ Y) = ¬X ⋀ ¬Y: ");
+System.Console.Write(!(x || y)==!x && !y);
