06978e56a711300ef509fbd4f0600da8acc00298
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-31 14:46:33 +0700

    Complete Example021

---
+++ Example021/Program.cs
@@ -0,0 +1,9 @@
+﻿//20. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+System.Console.Write("Введите число: ");
+int a = Int32.Parse(System.Console.ReadLine());
+if (a == 6 || a == 7) System.Console.WriteLine("Выходной");
+else
+{
+    if (a >= 1 && a <= 5) System.Console.WriteLine("Будний день");
+    else System.Console.WriteLine($"{a} не является днём недели");
+}
