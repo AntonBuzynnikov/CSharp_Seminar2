bb52b8b00f1608eec372fb0b230639f7ed3aad73
Buzynnikov <buzynnikov2111@gmail.com>
2023-01-31 14:31:54 +0700

    Complete Example020

---
+++ Example020/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите число: ");
+int a = Int32.Parse(Console.ReadLine());
+bool b1 = a%7 == 0;
+bool b2 = a%23 == 0;
+if (b1 && b2)
+{
+    System.Console.WriteLine($"{a} кратно 7 и 23");
+}
+else
+{
+    if (b1) System.Console.WriteLine($"{a} кратно 7");
+    else 
+    {
+        if (b2) System.Console.WriteLine($"{a} кратно 23");
+        else System.Console.WriteLine($"{a} не кратно ничему");
+    }
+}
